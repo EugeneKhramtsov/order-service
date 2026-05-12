0d6c525ba17199101802214c5ede738e36220e85
EugeneKhramtsov <yevheniikhramtsov@gmail.com>
2026-05-12 13:29:24 +0300

    Lab 6

--- src/main/java/uu/app/orderservice/controller/OrderController.java
+++ src/main/java/uu/app/orderservice/controller/OrderController.java
@@ -1,12 +1,13 @@
 package uu.app.orderservice.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 import uu.app.orderservice.entity.OrderEntity;
 import uu.app.orderservice.service.OrderService;
 
+@Slf4j
 @RestController
 @RequestMapping("/orders")
 @AllArgsConstructor
@@ -16,6 +17,7 @@ public class OrderController {
 
     @PostMapping
     public OrderEntity createOrder(@RequestBody OrderEntity order, @RequestHeader HttpHeaders headers) {
+        log.info("Creating order {}", order);
         return orderService.createOrder(order, headers);
     }
 
--- src/main/java/uu/app/orderservice/service/UserClient.java
+++ src/main/java/uu/app/orderservice/service/UserClient.java
@@ -1,7 +1,8 @@
 package uu.app.orderservice.service;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
